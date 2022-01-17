30fbda4286c9c08c77f9459350c0fe2b0337f84c
gehlotvishnu <47517136+gehlotvishnu@users.noreply.github.com>
2022-01-17 15:08:43 +0530

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
@@ -9,17 +7,6 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 CLASS_NAMES= ['Tomato___Bacterial_spot',
  'Tomato___Early_blight',
@@ -56,5 +43,3 @@ async def predict(
         "confidence": float(confidence)
     }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
