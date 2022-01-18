7f3f8b6cf185bd4265c10a1a2bf615a48764f054
gehlotvishnu <47517136+gehlotvishnu@users.noreply.github.com>
2022-01-18 20:13:38 +0530

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 CLASS_NAMES= ['Tomato___Bacterial_spot',
  'Tomato___Early_blight',
